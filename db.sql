6ad3e3d4e67cdf82f214b1af0fa81bd85af9051b
Jose Rodriguez <jrodriguez@icapitalnetwork.com>
2024-08-19 17:34:20 -0600

    Init commit

---
+++ db.sql
@@ -0,0 +1,20 @@
+-- POSTGRES_PASSWORD=password
+-- POSTGRES_HOST_AUTH_METHOD=trust
+
+CREATE DATABASE etl_pipelline;
+
+-- \c etl_pipeline;
+
+CREATE TABLE movies (
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+description VARCHAR(255),
+category VARCHAR(255)
+);
+
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+movie_id INTEGER,
+rating INTEGER,
+FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE SET NULL
+);
